f92c0c8da1bb1969a3e8bc16ad580a6e99c31607
fuad rivai <fuad.strummer@gmail.com>
2022-01-28 17:43:55 +0700

    fix barcode

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,6 +1,10 @@
 import 'package:barcode_widget/barcode_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bootstrap/flutter_bootstrap.dart';
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
+import 'package:printing/printing.dart';
+import 'dart:typed_data';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -11,8 +15,8 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   String? _dataCode = "1234567890";
-  final int _dataCount = 0;
-  final List<Widget> _barcodeWidget = [];
+  int _dataCount = 1;
+  Color _color = Colors.blue;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -57,12 +61,83 @@ class _HomeScreenState extends State<HomeScreen> {
                             BootstrapCol(
                               sizes: 'col-6 col-sm-6',
                               child: TextFormField(
+                                initialValue: _dataCount.toString(),
+                                onChanged: (d) {
+                                  if (d.isEmpty || d == "") {
+                                    _dataCount = 1;
+                                  } else if (d == "0") {
+                                    _dataCount = 1;
+                                  } else {
+                                    _dataCount = int.parse(d);
+                                  }
+                                  setState(() {});
+                                },
                                 keyboardType: TextInputType.number,
                               ),
                             )
                           ],
                         ),
-                      )
+                      ),
+                      BootstrapCol(
+                        sizes: 'col-12',
+                        child: const SizedBox(height: 20),
+                      ),
+                      BootstrapCol(
+                        sizes: 'col-12',
+                        child: BootstrapRow(
+                          children: [
+                            BootstrapCol(
+                              child: Center(
+                                child: TextButton(
+                                  onHover: (isHover) {
+                                    setState(() {
+                                      _color = isHover
+                                          ? Colors.blue[200]!
+                                          : Colors.blue;
+                                    });
+                                  },
+                                  style: TextButton.styleFrom(
+                                    backgroundColor: _color,
+                                  ),
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: const [
+                                      Icon(
+                                        Icons.print,
+                                        color: Colors.white,
+                                      ),
+                                      SizedBox(width: 5),
+                                      Text(
+                                        "Print",
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                  onPressed: () {
+                                    showDialog(
+                                        context: context,
+                                        builder: (context) {
+                                          return Dialog(
+                                            child: PdfPreview(
+                                              canChangePageFormat: false,
+                                              canChangeOrientation: false,
+                                              initialPageFormat:
+                                                  PdfPageFormat.a4,
+                                              canDebug: false,
+                                              build: (format) =>
+                                                  _generatePdf("Test"),
+                                            ),
+                                          );
+                                        });
+                                  },
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
                     ],
                   ),
                 ),
@@ -74,17 +149,17 @@ class _HomeScreenState extends State<HomeScreen> {
                       child: GridView.count(
                         shrinkWrap: true,
                         physics: const ScrollPhysics(),
-                        crossAxisCount: 5,
+                        crossAxisCount: 6,
                         childAspectRatio: 1.5,
                         crossAxisSpacing: 12,
-                        children: _barcodeWidget.map((e) {
+                        children: List.generate(_dataCount, (index) {
                           return BarcodeWidget(
                             barcode: Barcode.code128(escapes: true),
                             data: _dataCode ?? "",
                             width: 20,
                             height: 50,
                           );
-                        }).toList(),
+                        }),
                       ),
                     ),
                   ),
@@ -96,5 +171,33 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
       ),
     );
+  }
+
+  Future<Uint8List> _generatePdf(String title) async {
+    final pdf = pw.Document(version: PdfVersion.pdf_1_5, compress: true);
+
+    pdf.addPage(
+      pw.Page(
+        pageFormat: PdfPageFormat.a4,
+        build: (context) {
+          return pw.GridView(
+            crossAxisCount: 6,
+            childAspectRatio: 0.5,
+            crossAxisSpacing: 10,
+            children: List.generate(_dataCount, (index) {
+              return pw.BarcodeWidget(
+                barcode: Barcode.code128(escapes: false),
+                data: _dataCode ?? "",
+                width: 20,
+                height: 50,
+                textStyle: const pw.TextStyle(fontSize: 7),
+              );
+            }),
+          );
+        },
+      ),
+    );
+
+    return pdf.save();
   }
 }
